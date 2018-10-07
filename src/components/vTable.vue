db7ffb996914a00854c02d51f98584b8f1675c1c
Allen Liu <lhard@qq.com>
2018-10-07 20:20:20 +0800

    排序完成

--- src/components/vTable.vue
+++ src/components/vTable.vue
@@ -25,6 +25,7 @@ export default {
     makeColums () {
       this.currentColums = this.columns.map((col, index) => {
         col._index = index;
+        col._sortType = 'normal';
         return col;
       });
     },
@@ -34,13 +35,50 @@ export default {
         return row;
       });
     },
+    handleSort (index, type = 'asc') {
+      this.currentColums.forEach(col => {
+        col._sortType = 'normal';
+      })
+      this.currentColums[index]._sortType = type;
+      let key = this.currentColums[index].key;
+      this.currentData.sort((a, b) => {
+        if (type === 'asc') return a[key] > b[key];
+        else return a[key] < b[key];
+      });
+    },
   },
   render (h) {
     const ths = [];
     const trs = [];
 
-    this.currentColums.forEach(col => {
-      ths.push(h('th', col.name));
+    this.currentColums.forEach((col, index) => {
+      if (col.sortable) {
+        ths.push(h('th', [
+          h('span', col.name),
+          h('a', {
+            'class': {
+              'sort-type': col._sortType === 'asc'
+            },
+            on: {
+              click: () => {
+                this.handleSort(index);
+              },
+            },
+          }, '↑'),
+          h('a', {
+            class: {
+              'sort-type': col._sortType === 'desc',
+            },
+            on: {
+              click: () => {
+                this.handleSort(index, 'desc');
+              },
+            },
+          }, '↓'),
+        ]));
+      } else {
+        ths.push(h('th', col.name));
+      }
     });
 
     this.currentData.forEach(row => {
@@ -48,10 +86,10 @@ export default {
       this.currentColums.forEach(cell => {
         tds.push(h('td', row[cell.key]));
       });
-      console.log(tds);
+      // console.log(tds);
       trs.push(h('tr', tds));
     });
-    console.log(trs);
+
     return h('table', [
       h('thead', [
         h('tr', ths)
@@ -61,3 +99,29 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+table, th, td {
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+}
+table {
+  width: 600px;
+}
+th > a {
+  display: inline-block;
+  width: 15px;
+  margin-right: 5px;
+  cursor: pointer;
+}
+th > a:last-of-type {
+  margin-right: 0;
+}
+tr {
+  line-height: 30px;
+}
+.sort-type {
+  color: red;
+  font-weight: 800;
+}
+</style>
