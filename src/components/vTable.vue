dc4d8eeaa56c37d706413eb81cd69b528862b4df
Allen Liu <lhard@qq.com>
2018-10-10 16:07:56 +0800

    解决表格加上排序代码后排序符号加红可以显示，去掉排序代码有问题

--- src/components/vTable.vue
+++ src/components/vTable.vue
@@ -40,6 +40,14 @@ export default {
         col._sortType = 'normal';
       })
       this.currentColums[index]._sortType = type;
+
+      // 上行代码直接改变数组对象中_sortType的值，vue是监测不到它数据的变化的。使用下面三行代码可以实现监测数组的变化。关键字：this.$set
+      // 为什么加上排序代码，就可以监测到数组对象中键的变化？因为排序时表格重新渲染，执行render函数了，render函数中包含列数组的渲染。
+      // let orderColum = this.currentColums[index];
+      // orderColum._sortType = type;
+      // this.$set(this.currentColums, index, orderColum);
+
+      // 排序
       let key = this.currentColums[index].key;
       this.currentData.sort((a, b) => {
         if (type === 'asc') return a[key] > b[key];
